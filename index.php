d425159914ea7db79c91ab0ac03eba6f631c0e25
silviapetrino <petrino.silvia@gmail.com>
2023-11-06 14:47:18 +0100

    [add]: Movie class with array of its instances

--- index.php
+++ index.php
@@ -1,7 +1,33 @@
 <?php 
 
+// 1. creo una nuova classe movie con le relative proprietà
+
+class Movie {
+
+  public $title;
+  public $year;
+  public $description;
+  public $language;
+  public $cover;
+
+  public function __construct(string $_title, int $_year, string $_description){
+    $this->title = $_title;
+    $this->year = $_year;
+    $this->description = $_description;
+  }
+
+}
 
 
+// 2. creo un array con istanze della classe Movie
+
+$movies = [
+  new Movie('Willy Wonka & the Chocolate Factory', 1971, 'A fantastical musical film based on Roald Dahl \' s book. Gene Wilder plays the role of Willy Wonka, the eccentric owner of the chocolate factory, who opens its doors to a group of contest-winning children.'),
+  new Movie('Charlie and the Chocolate Factory', 2005, 'A more recent adaptation of Roald Dahl \' s book, directed by Tim Burton. Johnny Depp plays Willy Wonka in this version, which follows the original material more closely than the 1971 film.'),
+  new Movie('Wonka', 2023, 'The movie aims to be a prequel that narrates the origins of the character born from the pen of the writer Roald Dahl, retracing all the adventures faced in youth to fulfill the dream of opening a chocolate factory.')
+];
+
+var_dump($movies);
 
 ?>
 
@@ -16,6 +42,8 @@
   <title>Php-oop</title>
 </head>
 <body>
+
+  
   
 </body>
 </html>
