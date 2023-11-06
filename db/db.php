508bea8cc277559c76b2fddeb66fa75ab96a8c2d
silviapetrino <petrino.silvia@gmail.com>
2023-11-06 16:01:20 +0100

    [remove]: model folder

---
+++ db/db.php
@@ -0,0 +1,9 @@
+<?php
+
+// 2. creo un array con istanze della classe Movie
+
+$movies = [
+  new Movie('Willy Wonka & the Chocolate Factory', 1971, 'A fantastical musical film based on Roald Dahl \' s book. Gene Wilder plays the role of Willy Wonka, the eccentric owner of the chocolate factory, who opens its doors to a group of contest-winning children.', 'English', 'willy-wonka-the-chocolate-factory_a24690.jpg', 'extra extra extra' ),
+  new Movie('Charlie and the Chocolate Factory', 2005, 'A more recent adaptation of Roald Dahl \' s book, directed by Tim Burton. Johnny Depp plays Willy Wonka in this version, which follows the original material more closely than the 1971 film.', 'English', 'MV5BNjcxMjg1Njg2NF5BMl5BanBnXkFtZTcwMjQ4NzMzMw@@._V1_.jpg', 'extra extra extra'),
+  new Movie('Wonka', 2023, 'The movie aims to be a prequel that narrates the origins of the character born from the pen of the writer Roald Dahl, retracing all the adventures faced in youth to fulfill the dream of opening a chocolate factory.', 'English', 'Wonka-113186318-large.jpg', 'extra extra extra')
+];
